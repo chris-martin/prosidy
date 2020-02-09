7653bd72c763571c038ba1f9fd525a9009654559
Alex Feldman-Crough <alex@fldcr.com>
2020-02-09 12:11:34 -0800

    Fix the static hash

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -14,7 +14,7 @@ let
 
   staticUrl     = "https://github.com/nh2/nixpkgs/archive/${staticCommit}.tar.gz";
   staticCommit  = "0c960262d159d3a884dadc3d4e4b131557dad116";
-  staticSha256  = "1ky79vczvf2sv6gk234i2m4vsxz1wpkh69gm8c7s1qwyawar9vjp";
+  staticSha256  = "0d7ms4dxbxvd6f8zrgymr6njvka54fppph1mrjjlcan7y0dhi5rb";
 
   nixpkgs       = import tarball args;
   tarball       = 
