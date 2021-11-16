940eba441b6ea5e9ce3a008dddd6d602164c74dc
Chris Martin <ch.martin@gmail.com>
2021-11-16 14:33:23 -0700

    test with ghc 9.0

--- .github/workflows/test.hs
+++ .github/workflows/test.hs
@@ -11,18 +11,20 @@ main =
 x .= Just y  = Just ("--constraint=" ++ x ++ "==" ++ y)
 x .= Nothing = Nothing
 
-data GHC = GHC_8_6 | GHC_8_8 | GHC_8_10
+data GHC = GHC_8_6 | GHC_8_8 | GHC_8_10 | GHC_9_0
 
 readGHC s = case s of
     "8.6"  -> GHC_8_6
     "8.8"  -> GHC_8_8
     "8.10" -> GHC_8_10
+    "9.0"  -> GHC_9_0
 
 constraints ghc = catMaybes
     [ "base" .= case ghc of
           GHC_8_6  -> Just "4.12.*"
           GHC_8_8  -> Just "4.13.*"
           GHC_8_10 -> Just "4.14.*"
+          GHC_9_0  -> Just "4.15.*"
     , "aeson" .= case ghc of
           GHC_8_6  -> Just "1.4.*"
           GHC_8_8  -> Just "1.5.*"
