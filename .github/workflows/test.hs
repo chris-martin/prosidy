a576b3151f4cac31d46eeec2d50ca80a01e343f4
Chris Martin <ch.martin@gmail.com>
2021-11-16 14:43:54 -0700

    get more strict about CI constraints

--- .github/workflows/test.hs
+++ .github/workflows/test.hs
@@ -30,17 +30,30 @@ constraints ghc = catMaybes
           GHC_8_8  -> Just "1.5.*"
           GHC_8_10 -> Just "2.0.*"
           _        -> Nothing
+    , "base-compat-batteries" .= case ghc of
+          GHC_8_8  -> Just "0.11.*"
+          GHC_9_0  -> Just "0.12.*"
+          _        -> Nothing
     , "hashable" .= case ghc of
           GHC_8_8  -> Just "1.3.*"
           GHC_8_10 -> Just "1.4.*"
           _        -> Nothing
     , "megaparsec" .= case ghc of
+          GHC_8_6  -> Just "7.0.*"
           GHC_8_10 -> Just "9.0.1"
+          GHC_9_0  -> Just "9.2.*"
           _        -> Nothing
     , "prettyprinter" .= case ghc of
-          GHC_8_10 -> Just "1.7.0"
+          GHC_8_6  -> Just "1.6.*"
+          GHC_8_10 -> Just "1.7.*"
           _        -> Nothing
     , "profunctors" .= case ghc of
+          GHC_8_6  -> Just "5.3.*"
           GHC_8_10 -> Just "5.6.2"
+          _        -> Nothing
+    , "tasty" .= case ghc of
+          GHC_8_6  -> Just "1.2.*"
+          GHC_8_10 -> Just "1.3.*"
+          GHC_9_0  -> Just "1.4.*"
           _        -> Nothing
     ]
